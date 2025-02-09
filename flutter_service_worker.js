2632ec4dec2c25060708e64fa2647d498afd22b7
Alexis Picard <44199439+pickywawa@users.noreply.github.com>
2025-02-09 23:35:00 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9c4cd13cc5d56dab22d43a2f62619af7",
+const RESOURCES = {"flutter_bootstrap.js": "c6e90939c3a0f55d4c659f4be550a2fd",
 "version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "0daa6c1f21c0fe429ba9c277ba5d6f25",
-"/": "0daa6c1f21c0fe429ba9c277ba5d6f25",
-"main.dart.js": "c07a00caf665bbef92464f49dc35bcfb",
+"index.html": "0ccd5becaae8fe81699bde9927ca688a",
+"/": "0ccd5becaae8fe81699bde9927ca688a",
+"main.dart.js": "267b2a7983636c6eee3e785b1335a9dd",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
