7a01f0d6115471197af5604ec7589ce2852197f0
Alexis Picard <44199439+pickywawa@users.noreply.github.com>
2025-09-22 00:53:39 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2892295034"
+    serviceWorkerVersion: "2365766229"
   }
 });
